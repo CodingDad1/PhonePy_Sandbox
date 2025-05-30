1e1e77a3ba5484c72a9fecdb67bc757e96a4bf38
CodingDad1 <nickjrodriguez02@gmail.com>
2025-05-30 15:22:57 -0500

    Add Calculatory.py from the right damn folder

---
+++ Calculator.py
@@ -0,0 +1,30 @@
+sign = input("Enter your mathmatical sign here: ")
+num1 = input("Enter your first number: ")
+num2 = input("Enter your second number: ")
+
+if sign == "+":
+    try:
+        sum_result = int(num1) + int(num2)
+        print(f"{num1} + {num2} = ", sum_result)
+    except ValueError:
+        print("Invalid input you twat.")
+elif sign == "-":
+    try:
+        minus_res = int(num1) - int(num2)
+        print(f"{num1} - {num2} = ", minus_res)
+    except ValueError:
+        print("Invalid input, seems like user error to me.")
+elif sign == "*":
+    try:
+        times_res = int(num1) * int(num2)
+        print(f"{num1} * {num2} = ", times_res)
+    except ValueError:
+        print("Bruh how do you fuck this up. Invalid Input...")
+elif sign == "/":
+    try:
+        div_res = int(num1) / int(num2)
+        print(f"{num1} / {num2} = ", div_res)
+    except ValueError:
+        print("Ok division is kind hard.. but try to enter a number next time")
+else:
+    print("Your sign isn't: +, -, * or / come on, its basic math.")
