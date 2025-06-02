888bdbcc8819bf838d3e5db35020dc3feddfdd70
CodingDad1 <nickjrodriguez02@gmail.com>
2025-06-02 18:17:35 -0500

    Adding Comments and While Loop Functionality

--- Calculator.py
+++ Calculator.py
@@ -1,39 +1,45 @@
-
-# inputs for 1st number operator and 2nd number
-num1 = input("Enter your first number: ")
-sign = input("Enter your mathmatical sign here: ")
-num2 = input("Enter your second number: ")
+while True:
+    # inputs for 1st number operator and 2nd number
+    num1 = input("Enter your first number: ")
+    sign = input("Enter your mathmatical sign here: ")
+    num2 = input("Enter your second number: ")
 
 # math +
-if sign == "+":
-    try:
-        sum_result = float(num1) + float(num2)
-        print(f"{num1} + {num2} = ", sum_result)
-    except ValueError:
-        print("Invalid input you twat.")
+    if sign == "+":
+        try:
+            sum_result = float(num1) + float(num2)
+            print(f"{num1} + {num2} = ", sum_result)
+        except ValueError:
+            print("Invalid input you twat.")
 
-# math -
-elif sign == "-":
-    try:
-        minus_res = float(num1) - float(num2)
-        print(f"{num1} - {num2} = ", minus_res)
-    except ValueError:
-        print("Invalid input, seems like user error to me.")
+    # math -
+    elif sign == "-":
+        try:
+            minus_res = float(num1) - float(num2)
+            print(f"{num1} - {num2} = ", minus_res)
+        except ValueError:
+            print("Invalid input, seems like user error to me.")
 
-# math *
-elif sign == "*":
-    try:
-        times_res = float(num1) * float(num2)
-        print(f"{num1} * {num2} = ", times_res)
-    except ValueError:
-        print("Bruh how do you fuck this up. Invalid Input...")
+    # math *
+    elif sign == "*":
+        try:
+            times_res = float(num1) * float(num2)
+            print(f"{num1} * {num2} = ", times_res)
+        except ValueError:
+            print("Bruh how do you fuck this up. Invalid Input...")
 
-# math / and last else statement for anything but numbers
-elif sign == "/":
-    try:
-        div_res = float(num1) / float(num2)
-        print(f"{num1} / {num2} = ", div_res)
-    except ValueError:
-        print("Ok division is kind hard.. but try to enter a number next time")
-else:
-    print("Your sign isn't: +, -, * or / come on, its basic math.")
+    # math / and last else statement for anything but numbers
+    elif sign == "/":
+        try:
+            div_res = float(num1) / float(num2)
+            print(f"{num1} / {num2} = ", div_res)
+        except ValueError:
+            print("Ok division is kind hard.. but try to enter a number next time")
+    else:
+        print("Your sign isn't: +, -, * or / come on, its basic math.")
+
+#User choice to restart function/Calculator
+    cont = input("Continue with another equation? (y/n): ").strip().lower()
+    if cont != "y":
+        print("Until the next session, Until the next equation, the calculator sleeps....")
+        break
